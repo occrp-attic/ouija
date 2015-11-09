e0617a5c28d76b95533a25bb2b78a7523cbc62d2
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-09 19:58:14 +0100

    make webapp work

--- ouija/core.py
+++ ouija/core.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.assets import Environment
+from sqlalchemy import create_engine
 
 from ouija import default_settings
 
@@ -7,3 +8,5 @@ app = Flask('ouija')
 app.config.from_object(default_settings)
 app.config.from_envvar('OUIJA_SETTINGS', silent=True)
 assets = Environment()
+
+engine = create_engine(app.config.get('DATABASE_URI'))
---
+++ ouija/default_settings.py
@@ -0,0 +1,3 @@
+
+DEBUG = True
+DATABASE_URI = 'postgresql://localhost/datavault'
