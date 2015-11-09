a89d8ec403ca1a71b69573d16bb8db082d60d5f9
Friedrich Lindenberg <friedrich@pudo.org>
2015-11-09 20:38:00 +0100

    make the api do more shit

--- ouija/api.py
+++ ouija/api.py
@@ -1,32 +1,68 @@
 from flask import render_template, request, Blueprint
 from apikit import jsonify
+from sqlalchemy import Table
+from werkzeug.exceptions import NotFound
 
-from ouija.core import engine, metadata
+from ouija.core import engine, metadata, url_for
 from ouija.util import angular_templates
-
-# TODO: make notes, bookmarks, links
 
 base_api = Blueprint('base', __name__)
 
 
-def table_data(table):
-    return {
-        'name': table.name
+def normalize_column_type(column):
+    name = unicode(column.type).lower()
+    if name.startswith('varchar') or name.startswith('char'):
+        return 'text'
+    if name.startswith('numeric'):
+        return 'float'
+    return name
+
+
+def table_data(table, detailed=True):
+    data = {
+        'name': table.name,
+        'label': table.name,
+        'metadata_uri': url_for('base.tables_view', name=table.name),
+        'rows_uri': url_for('base.tables_rows', name=table.name),
+        'columns_num': len(table.columns)
     }
+    if detailed or True:
+        data['columns'] = []
+        for column in table.columns:
+            data['columns'].append({
+                'name': column.name,
+                'label': column.name,
+                'type': normalize_column_type(column)
+            })
+    return data
 
 
 @base_api.route('/api/tables')
 def tables_index():
     tables = []
     for table in metadata.tables.values():
-        tables.append(table_data(table))
+        tables.append(table_data(table, detailed=False))
     return jsonify({
         'tables': tables,
         'table_count': len(tables)
     })
 
 
+@base_api.route('/api/table/<name>')
+def tables_view(name):
+    if name not in metadata.tables.keys():
+        raise NotFound()
+    table = Table(name, metadata, autoload=True, autoload_with=engine)
+    return jsonify(table_data(table))
+
+
+@base_api.route('/api/table/<name>/rows')
+def tables_rows(name):
+    return jsonify({
+    })
+
+
 @base_api.route('/')
 def index():
-    templates = []  # angular_templates()
+    templates = angular_templates()
     return render_template('html/index.html', templates=templates)
--- ouija/core.py
+++ ouija/core.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, url_for as _url_for
 from flask.ext.assets import Environment
 from sqlalchemy import create_engine, MetaData
 
@@ -16,3 +16,14 @@ assets = Environment(app)
 engine = create_engine(app.config.get('DATABASE_URI'))
 metadata = MetaData(bind=engine)
 metadata.reflect(engine)
+
+
+def url_for(*a, **kw):
+    """ Always generate external URLs. """
+    try:
+        kw['_external'] = True
+        # if app.config.get('PREFERRED_URL_SCHEME'):
+        #    kw['_scheme'] = app.config.get('PREFERRED_URL_SCHEME')
+        return _url_for(*a, **kw)
+    except RuntimeError:
+        return None
