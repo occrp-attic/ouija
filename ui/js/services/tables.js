640ff516a20ebc1bf0594814c7ba7cc1e701f0af
Smári McCarthy <smari@occrp.org>
2015-11-09 20:30:41 +0100

    Adding tables service

---
+++ ui/js/services/tables.js
@@ -0,0 +1,50 @@
+
+ouija.factory('tables', ['$route', '$location', '$q', '$http', '$analytics',
+    function($route, $location, $q, $http, $analytics) {
+  var query = {};
+
+  var ensureArray = function(data) {
+    if (!angular.isDefined(data)) {
+      return [];
+    }
+    return data;
+  };
+
+  var get = function() {
+    query = {};
+    return query;
+  };
+
+  var set = function(name, val) {
+    query = get();
+    query[name] = val;
+    $location.path('/tables').search(query);
+  }
+
+  var clear = function() {
+    $location.search({});
+  };
+
+  var execute = function() {
+    var dfd = $q.defer(),
+        query = get();
+
+    $http.get('/api/tables', {}).then(function(res) {
+      dfd.resolve(res.data);
+    }, function(err) {
+      dfd.reject(err);
+    });
+    return dfd.promise;
+  };
+
+  get();
+
+  return {
+      state: query,
+      get: get,
+      set: set,
+      clear: clear,
+      execute: execute,
+  };
+
+}]);
